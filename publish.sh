4d7ab7049ef8805ef57ed15b82aafc829c8422be
Natalie Bridgers <natalie@stream.place>
2026-04-10 16:39:43 -0500

    ya

--- publish.sh
+++ publish.sh
@@ -8,6 +8,13 @@ VERSION=$(grep '^version' tweezer/Cargo.toml | head -1 | sed 's/.*"\(.*\)"/\1/')
 echo "Waiting for crates.io to index tweezer $VERSION..."
 sleep 10
 
+echo "Updating tweezer-streamplace dep to $VERSION..."
+sed -i '' "s/tweezer = { path = \"..\/tweezer\", version = \"[^\"]*\" }/tweezer = { path = \"..\/tweezer\", version = \"$VERSION\" }/" tweezer-streamplace/Cargo.toml
+
+# commit the updated version to git
+git add tweezer-streamplace/Cargo.toml
+git commit -m "Update tweezer deps to $VERSION"
+
 echo "Publishing tweezer-streamplace..."
 cargo publish -p tweezer-streamplace
 
